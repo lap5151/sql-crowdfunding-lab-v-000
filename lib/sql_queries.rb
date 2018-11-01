05e475fb2da7315f403ac496c406b757c0c3bdbf
Lauren Pardee <lap5151@gmail.com>
2018-11-01 14:22:28 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -10,7 +10,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 "SELECT projects.title, pledges.amount FROM projects
 INNER JOIN pledges ON pledges.project_id = projects.id
 GROUP BY projects.title
-ORDER BY pledges.amount DESC
+ORDER BY projects.title DESC
 "
 end
 
